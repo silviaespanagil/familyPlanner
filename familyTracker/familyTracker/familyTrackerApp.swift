0cd2e4199a88e352be2d789907e9d573353ebd5c
Silvia España <silviaespanagil@gmail.com>
2024-02-02 16:37:14 +0100

    First commit

---
+++ familyTracker/familyTracker/familyTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  familyTrackerApp.swift
+//  familyTracker
+//
+//  Created by Silvia España Gil on 2/2/24.
+//
+
+import SwiftUI
+
+@main
+struct familyTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
